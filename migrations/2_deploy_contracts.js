27d2147743041b0a7b3615725c63a82d604cf7d4
Stéphane-Marcel KOUAME <stephmarcel.kouame@gmail.com>
2019-01-21 16:12:14 +0000

    StephCoin V1 -> Nouvelle version à utiliser pour continuer

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,13 +4,16 @@
 //   deployer.deploy(SMKCoinERC20);
 // };
 
-var SMKCoin = artifacts.require("./SMKCoin.sol");
+// var SMKCoin = artifacts.require("./SMKCoin.sol");
+//
+// module.exports = function(deployer) {
+// const _name = "SMKCoin";
+// const _symbol = "SMK";
+// const _decimals = 18;
+//
+//  deployer.deploy(SMKCoin, _name, _symbol, _decimals);
 
+var StephCoin = artifacts.require("./StephCoin.sol");
 module.exports = function(deployer) {
-const _name = "SMKCoin";
-const _symbol = "SMK";
-const _decimals = 18;
-
- deployer.deploy(SMKCoin, _name, _symbol, _decimals);
-
+ deployer.deploy(StephCoin);
 };
